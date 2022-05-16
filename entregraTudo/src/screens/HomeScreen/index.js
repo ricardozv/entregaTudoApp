e1ee5990c4de349eb100c013152df910cd0c8e5d
zumb <zumb@zumb.zumb>
2022-05-16 15:32:15 -0300

    create component DishListItem

--- entregraTudo/src/screens/HomeScreen/index.js
+++ entregraTudo/src/screens/HomeScreen/index.js
@@ -1,18 +1,24 @@
-import { StyleSheet, FlatList } from 'react-native';
-import RestaurantItem from "../../../src/components";
+import { StyleSheet, FlatList, View } from 'react-native';
+import RestaurantItem from "../../components/RestaurantItem";
 import restaurants from "../../../assets/data/restaurants.json";
 
 export default function App() {
   return (
+    <View style = {styles.page}>
       <FlatList 
         data = {restaurants} 
         renderItem={({item})=> <RestaurantItem restaurant={item} /> } 
         showsVerticalScrollIndicator = {false}
       />
+     </View>
      
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    padding: 10,
+  }
+});
 
 
