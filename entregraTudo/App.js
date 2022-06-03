82148639c43d61365c09304e725198d59812e5e1
zumb <zumb@zumb.zumb>
2022-06-03 11:39:23 -0300

    add aws amplify

--- entregraTudo/App.js
+++ entregraTudo/App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './src/navigation';
+
 import { Amplify } from 'aws-amplify'
 import awsconfig from './src/aws-exports'
 Amplify.configure(awsconfig)
+
 
 
 export default function App() {
