0a53f406a40a90678d96469676a201f7b0ec6699
zumb <zumb@zumb.zumb>
2022-06-24 20:50:24 -0300

    create data model on aws amplify, graphql api

--- entregraTudo/App.js
+++ entregraTudo/App.js
@@ -1,26 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigation';
-
-import { Amplify } from 'aws-amplify'
+import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from "aws-amplify-react-native";
-import config from './src/aws-exports'
+import RootNavigator from './src/navigation';
+import config from './src/aws-exports';
+
 Amplify.configure({...config,
 Analytics:{
   disabled: true
 }});
 
-
-
- function App() {
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
       <StatusBar style="auto" />
     </NavigationContainer>
   );
-}
+};
 
-export default withAuthenticator(App);
+export default withAuthenticator(App, true);
 
 
--- entregraTudo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ entregraTudo/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "entregaTudo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
