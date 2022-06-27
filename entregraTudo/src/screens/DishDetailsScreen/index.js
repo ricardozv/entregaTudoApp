4e3f97b52343774cb5bfc2d186ffa0f084c99705
zumb <zumb@zumb.zumb>
2022-06-27 19:53:02 -0300

    uploading

--- entregraTudo/src/screens/DishDetailsScreen/index.js
+++ entregraTudo/src/screens/DishDetailsScreen/index.js
@@ -13,10 +13,12 @@ const DishDetailsScreen = () => {
 
     const navigation = useNavigation();
     const route = useRoute();
-    const id = route.params.id;
+    const id = route.params?.id;
 
     useEffect(() => {
+        if(id) {
         DataStore.query(Dish, id).then(setDish);
+        }
     }, [])
 
     const onMinus = () => {
--- entregraTudo/src/screens/RestaurantDetailsScreen/index.js
+++ entregraTudo/src/screens/RestaurantDetailsScreen/index.js
@@ -18,11 +18,15 @@ const RestaurantDetailsScreen = () => {
     const id = route.params?.id;
 
     useEffect(() => {
+        if(!id) {
+            return;
+        }
+
         DataStore.query(Restaurant, id).then(setRestaurant);
         DataStore.query( Dish, (dish) => dish.restaurantID("eq", id )).then(
             setDishes
-        )
-    },[])
+        );
+    },[id]);
 
     if (!restaurant) {
         return (< ActivityIndicator size={"large"} color="gray"/>)
