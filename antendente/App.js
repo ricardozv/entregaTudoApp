f76958cbb50f4f2cf2a520b93dad2dd3b897447a
zumb <zumb@zumb.zumb>
2022-07-12 01:09:40 -0300

    create ordersScreen and orderDelivery on screen

--- antendente/App.js
+++ antendente/App.js
@@ -1,10 +1,9 @@
 import { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
 import orders from './assets/data/orders.json';
-import OrderItem from './src/components/OrderItem';
 import OrdersScreen from './src/screens/OrdersScreen';
+import OrderDelivery from './src/screens/OrderDelivery';
 
 const order = orders[0]
 
@@ -12,12 +11,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-     {/* <FlatList 
-        data ={orders}
-        renderItem={({item})=> 
-        <OrderItem order={item} />} 
-        /> */}
-        <OrdersScreen />
+     
+       {/* <OrdersScreen /> */}
+        <OrderDelivery /> 
     </View>
   );
 }
