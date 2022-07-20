0975520c359000fbd6bad8f34bbde973c4f5591a
zumb <zumb@zumb.zumb>
2022-07-20 15:30:25 -0300

    add aws cognito

--- antendente/App.js
+++ antendente/App.js
@@ -2,10 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import orders from './assets/data/orders.json';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './src/navigation';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure({ 
+  ...awsconfig, 
+Analytics: {
+  disabled: true
+},});
 
 const order = orders[0]
 
-export default function App() {
+ function App() {
   return (
     <NavigationContainer>
               <Navigation/>
@@ -14,4 +23,4 @@ export default function App() {
   );
 }
 
-
+export default withAuthenticator(App);
